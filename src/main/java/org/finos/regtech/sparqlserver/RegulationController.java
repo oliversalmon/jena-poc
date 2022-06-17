a80a02daecf7cdcca24de78e77bd1b137e6b69c3
oliver.salmon@gmail.com <XerxesUK11!>
2022-06-17 14:20:40 +0100

    First go at Turtle

--- src/main/java/org/finos/regtech/sparqlserver/RegulationController.java
+++ src/main/java/org/finos/regtech/sparqlserver/RegulationController.java
@@ -4,10 +4,16 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+
 
 @RestController
 public class RegulationController {
 
     Model regModel = ModelFactory.createDefaultModel();
+
+    public void loadData(File schema){
+
+    }
 
 }
